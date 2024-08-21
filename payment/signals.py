e5a4690e9d94c21d794f639777873e97dafef299
Bastian8004 <123553744+Bastian8004@users.noreply.github.com>
2024-08-21 17:58:09 +0200

    Update signals.py

--- payment/signals.py
+++ payment/signals.py
@@ -10,16 +10,10 @@ def payment_notification(sender, **kwargs):
     ipn_obj = sender
     if ipn_obj.payment_status == ST_PP_COMPLETED:
 
-        # Check that the receiver email is the same we previously
-        # set on the `business` field. (The user could tamper with
-        # that fields on the payment form before it goes to PayPal)
         if ipn_obj.receiver_email != settings.PAYPAL_RECEIVER_EMAIL:
-            # Not a valid payment
+            
             return
-
-        # payment was successful
         order = get_object_or_404(Order, id=ipn_obj.invoice)
-        # mark the order as paid
         order.paid = True
         order.save()
 
