d0073080f758bbe9210735c2e10b1524d1b4db41
Bastian8004 <123553744+Bastian8004@users.noreply.github.com>
2024-08-21 17:53:06 +0200

    Update task.py

--- orders/task.py
+++ orders/task.py
@@ -4,17 +4,14 @@ from .models import Order
 
 
 def order_created(order_id):
-    """
-    Task to send an e-mail notification when an order is
-    successfully created.
-    """
+
     order = Order.objects.get(id=order_id)
-    subject = 'Order number {}'.format(order.id)
-    message = 'Dear {},\n\nYou have successfully placed an order.\
-                Your order id is {}.'.format(order.first_name,
+    subject = 'Zamówienie numer {}'.format(order.id)
+    message = 'Drogi {},\n\nUkończyłeś właśnie swojje zamówienie.\
+                Twój numer zamówienia to: {}.'.format(order.first_name,
                                              order.id)
     mail_sent = send_mail(subject,
                           message,
-                          'django-shop-tutorial@myshop.com',
+                          'bastastore@gmail.com',
                           [order.email])
     return mail_sent
